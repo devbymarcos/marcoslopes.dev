7a730c3dde3bd8299d9be5e535ededf270f66140
Marcos <marcoslopes.dev@gmail.com>
2023-06-29 16:41:35 -0300

    add arquivos e header

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,14 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Marcos Lopes",
+  title: "Marcos Lopes | javascript developer",
   description: "Ola você chegou em meu site pessoal veja meus trabalhos",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-Br">
-      <body>{children}</body>
+      <body className="">{children}</body>
     </html>
   );
 }
