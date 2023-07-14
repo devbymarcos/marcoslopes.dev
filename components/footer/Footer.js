ed780144f6c12b6f6896540b0f6ba5c813dc7ddb
Marcos <marcoslopes.dev@gmail.com>
2023-07-14 18:06:42 -0300

    reafactor: corrigido erro de escrita

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -3,7 +3,7 @@ export default function Footer(options) {
     <footer className="w-full h-44">
       <div className="container">
         <div className="text-center text-white">
-          <p>Copyright &copy; 20223</p>
+          <p>Copyright &copy; 2023</p>
         </div>
       </div>
     </footer>
