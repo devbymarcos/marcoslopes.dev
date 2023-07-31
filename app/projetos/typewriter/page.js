687f0f28c6fe5e1acddf2639617821e371f3e3ee
Marcos <marcoslopes.dev@gmail.com>
2023-07-31 13:07:26 -0300

    refactor: remocao de linhas não utilizadas

--- app/projetos/typewriter/page.js
+++ app/projetos/typewriter/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
+
 import { Github } from "lucide-react";
 import BtnLinkFooterPage from "@/components/footer-page/BtnLinkFooterPage";
 
