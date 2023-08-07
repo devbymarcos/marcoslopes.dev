1ff2f59aadc1cc4e8aafd2795db443e7b98ed25e
Marcos <marcoslopes.dev@gmail.com>
2023-08-07 08:47:08 -0300

    delete: removido yarn

--- components/buttons/BtnLink.js
+++ components/buttons/BtnLink.js
@@ -4,7 +4,7 @@ export default function BtnLink({ children, href, title }) {
   return (
     <>
       <Link
-        className="py-4 px-6 border-2 border-color-purple hover:bg-color-purple text-white font-bold   rounded-md uppercase"
+        className="py-4 px-6 border-2 mt-3 border-color-purple hover:bg-color-purple text-white font-bold inline-block   rounded-md uppercase"
         href={href}
         target="_bank"
         title={title}
