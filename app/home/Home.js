c30fb2ac380681ae84fe58b6f0c006e11f96d66d
Marcos <marcoslopes.dev@gmail.com>
2023-08-14 16:36:21 -0300

    feat: contato via whatsapp

--- app/home/Home.js
+++ app/home/Home.js
@@ -22,12 +22,21 @@ export default function Home() {
             <span className="text-color-purple font-bold  ">Front-end</span>,
             construindo o Frontend de sites e aplicativos da Web
           </p>
-          <Link
-            className="px-5 py-4 text-lg border-2 transition-all inline-block border-color-purple rounded-md font-bold text-white hover:bg-color-purple uppercase"
-            href="/#contato"
-          >
-            Mande uma mensagem
-          </Link>
+          <div className="flex gap-3 items-center ">
+            <Link
+              className="px-5 py-4 text-md border-2 transition-all inline-block border-color-purple rounded-full font-bold text-white hover:bg-color-purple uppercase"
+              href="/#contato"
+            >
+              Mande uma mensagem
+            </Link>
+            <a
+              target="_blank"
+              className="px-5 py-4 text-md border-2 transition-all inline-block border-green-500 rounded-full font-bold text-white hover:bg-green-500 uppercase"
+              href="https://api.whatsapp.com/send?phone=5541992169810&text=Tenho%20trabalho%20vamos%20conversar"
+            >
+              whatsapp
+            </a>
+          </div>
         </div>
         <div className="relative w-[300px] overflow-hidden rounded-full hover:shadow-md h-[300px] justify-self-center">
           <Image
