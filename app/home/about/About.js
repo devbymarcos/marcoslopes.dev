57a3db09d6fe45517815d77a23595b963e391622
Marcos <marcoslopes.dev@gmasil.com>
2023-09-18 16:30:27 -0300

    add: icon e title SOBRE

--- app/home/about/About.js
+++ app/home/about/About.js
@@ -4,6 +4,7 @@ export default function About() {
   return (
     <>
       <section id="sobre" className="pt-40">
+        <h1 className="mb-12 text-white uppercase text-2xl font-bold">Sobre</h1>
         <MyDesc />
       </section>
     </>
