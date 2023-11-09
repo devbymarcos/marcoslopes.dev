89511a6ed1ca5ec49d5ed1f0cd9a2f7ca4b20409
Marcos <marcoslopes.dev@gmail.com>
2023-11-09 14:54:58 -0300

    style: create component blur home

--- src/app/home/Home.js
+++ src/app/home/Home.js
@@ -1,38 +1,25 @@
 import Link from "next/link";
-import Nav from "@/components/menu/Nav";
-
+import { BlurHome } from "@/app/home/blur/blurHome";
 export default function Home() {
   return (
     <>
-      <section className=" md:fixed w-[400px]  px-2 gap-3 pt-16  ">
-        <div className="  ">
-          <h1
-            className={`text-white text-5xl md:text-[45px] font-black uppercase `}
+      <section className="pt-28  ">
+        <BlurHome />
+        <div className="flex gap-3 items-center ">
+          <Link
+            className="px-5 py-3 text-md border-2 transition-all inline-block border-color-purple rounded-full font-bold text-white hover:bg-color-purple uppercase"
+            href="/#contato"
           >
-            Marcos Juvêncio
-          </h1>
-          <p className={` text-white text-xl md:text-[20px]   my-6 leading-7`}>
-            Um desenvolvedor JavaScript atualmente focado em{" "}
-            <span className="text-color-purple font-bold  ">Front-end</span>,
-            construindo o Frontend de sites e aplicativos da Web
-          </p>
-          <div className="flex gap-3 items-center ">
-            <Link
-              className="px-5 py-3 text-md border-2 transition-all inline-block border-color-purple rounded-full font-bold text-white hover:bg-color-purple uppercase"
-              href="/#contato"
-            >
-              Mensagem
-            </Link>
-            <a
-              target="_blank"
-              className="px-5 py-3 text-md border-2 transition-all inline-block border-green-500 rounded-full font-bold text-white hover:bg-green-500 uppercase"
-              href="https://api.whatsapp.com/send?phone=5541992169810&text=Tenho%20trabalho%20vamos%20conversar"
-            >
-              whatsapp
-            </a>
-          </div>
+            Mensagem
+          </Link>
+          <a
+            target="_blank"
+            className="px-5 py-3 text-md border-2 transition-all inline-block border-green-500 rounded-full font-bold text-white hover:bg-green-500 uppercase"
+            href="https://api.whatsapp.com/send?phone=5541992169810&text=Tenho%20trabalho%20vamos%20conversar"
+          >
+            whatsapp
+          </a>
         </div>
-        <Nav />
       </section>
     </>
   );
---
+++ src/app/home/blur/blurHome.js
@@ -0,0 +1,7 @@
+export const BlurHome = () => {
+  return (
+    <>
+      <div className="bg-[#111819] w-[900px] h-[800px] rounded-full blur-[4rem] absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 "></div>
+    </>
+  );
+};
