f9481f79e54d858d0b72b5f2942a0242623d26f6
Marcos <marcoslopes.dev@gmail.com>
2023-11-09 16:51:04 -0300

    style: new icon

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,25 @@
 import About from "./home/about/About";
 import { Contact } from "./home/contact/Contact";
 import Home from "./home/Home";
-import Stacks from "./home/stacks/Stacks";
+
+export const metadata = {
+  title: "devbymarcos | Prgramador Web",
+  description:
+    "Um desenvolvedor Web FullStack, criador de soluções computacionais para web",
+  alternates: {
+    canonical: "/",
+  },
+  openGraph: {
+    title: "Marcos | devbymarcos",
+    description:
+      "Um desenvolvedor Web FullStack, criador de soluções computacionais para web",
+    url: "https://devbymarcos.com.br",
+    siteName: "Next.js",
+    images: "/screenshot.png",
+    locale: "pt-BR",
+    type: "website",
+  },
+};
 
 export default function PageIndex() {
   return (
