a73cdfa11b1b03b3a3149c8bd417e4646d3821f2
devbymarcos <marcoslopes.dev@gmail.com>
2024-05-30 19:15:07 -0300

    refactor: navegação style

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -18,24 +18,24 @@ export default function Header() {
   }
 
   useEffect(() => {
-    // window.addEventListener("scroll", () => {
-    //   if (window.innerWidth > 769) {
-    //     const scroll = window.scrollY;
-    //     const header = document.querySelector(".header-nav");
-    //     if (scroll > 100) {
-    //       header.classList.remove("py-8");
-    //       header.classList.add("py-3");
-    //     } else {
-    //       header.classList.remove("py-3");
-    //       header.classList.add("py-8");
-    //     }
-    //   }
-    // });
+    window.addEventListener("scroll", () => {
+      if (window.innerWidth > 769) {
+        const scroll = window.scrollY;
+        const header = document.querySelector(".header-nav");
+        if (scroll > 100) {
+          header.classList.remove("py-12");
+          header.classList.add("py-3");
+        } else {
+          header.classList.remove("py-3");
+          header.classList.add("py-12");
+        }
+      }
+    });
   });
 
   return (
-    <header className="header-nav  fixed top-0 transition-all bg-primary-light dark:bg-primary-dark z-40 px-3 border-b border-blue-custom-light   w-full h-[55px] items-center   ">
-      <div className=" flex items-center gap-10 h-[55px] ">
+    <header className="header-nav  fixed top-0 transition-all bg-primary-light dark:bg-primary-dark z-40 px-3    w-full  items-center  py-12 ">
+      <div className=" flex items-center  gap-10 container-custom  ">
         <Link
           href="/"
           className={`${firaCode.className} flex justify-between  gap-4 items-center md:w-[300px] font-bold text-xl text-blue-custom-light pl-8 `}
@@ -45,15 +45,12 @@ export default function Header() {
         </Link>
         <BtnMobile onClick={openMenuMobile} open={menuMobile} />
         <nav className="hidden md:block">
-          <ul className="flex justify-center items-center last:border-r last:border-blue-custom-light  ">
+          <ul className="flex justify-center items-center  ">
             {menuData.map((nav) => {
               return (
-                <li
-                  className="border-l border-blue-custom-light h-[55px]"
-                  key={nav.title}
-                >
+                <li className=" " key={nav.title}>
                   <Link
-                    className={` ${firaCode.className}  px-11 block  border-blue-custom-light text-font-color dark:text-font-color-dark   text-sm hover:text-pink-custom py-4 `}
+                    className={` ${firaCode.className} hover:text-font-color px-11 block   dark:text-font-color-dark   text-sm  `}
                     href={nav.path}
                   >
                     {nav.title}
